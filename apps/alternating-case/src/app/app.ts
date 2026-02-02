dff216b1ea1d9f907badf5a997a9dd4ca85b7006
James Stiehl <jstiehl@gmail.com>
2026-02-02 12:41:45 -0800

    feat: add a header component and update some styling

--- apps/alternating-case/src/app/app.ts
+++ apps/alternating-case/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AlternatingCaseComponent } from './alternating-case/alternating-case.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
-  imports: [AlternatingCaseComponent, RouterModule],
+  imports: [AlternatingCaseComponent, HeaderComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
